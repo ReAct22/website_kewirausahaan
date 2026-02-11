6082f7f060c050fae16ae1d5006c849a865cd290
Fathir <115683702+allferyss@users.noreply.github.com>
2026-02-11 19:28:05 +0700

    Update 2026_02_10_143323_create_transactions_table.php

--- database/migrations/2026_02_10_143323_create_transactions_table.php
+++ database/migrations/2026_02_10_143323_create_transactions_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('kode_transaksi')->unique();
+            $table->unsignedBigInteger('id_barang');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('qty');
+            $table->decimal('harga_barang', 12, 2);
+            $table->decimal('total_harga', 12, 2);
+            $table->enum('jenis_bayar', ['cash', 'transfer', 'qris', 'debit', 'kredit']);
             $table->timestamps();
         });
     }
