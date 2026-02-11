a0cc8af9c0ea3968e1bb1c8876530489d7f7bb9b
Tsabitdly <tsabitdly@gmail.com>
2026-02-11 11:22:56 +0700

    Update 2026_02_10_143202_create_stores_table.php

--- database/migrations/2026_02_10_143202_create_stores_table.php
+++ database/migrations/2026_02_10_143202_create_stores_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade');
+            $table->string('nama_toko', 100);
+            $table->text('deskripsi');
             $table->timestamps();
         });
     }
